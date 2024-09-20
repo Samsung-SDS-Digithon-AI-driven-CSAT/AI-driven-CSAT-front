43f3bfe9eca89842624842be2c40c771cfef3b91
sahil-shroff <sahilshroff2@gmail.com>
2024-09-20 10:42:49 +0530

    feat: vuetify added

--- AI-driven-CSAT-front/src/main.ts
+++ AI-driven-CSAT-front/src/main.ts
@@ -2,4 +2,25 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import Vuetify
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles'; // Import Vuetify CSS
+
+// Import Vuetify Icon styles
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
+  });
+
+createApp(App).use(vuetify).mount('#app');
